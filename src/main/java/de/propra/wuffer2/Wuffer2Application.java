281d404f7452a0bfc901925ca59b4f3c19b60307
Sascha Klassen <102946831+sakla106@users.noreply.github.com>
2023-02-07 12:30:52 +0100

    init

---
+++ src/main/java/de/propra/wuffer2/Wuffer2Application.java
@@ -0,0 +1,14 @@
+package de.propra.wuffer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("checkstyle:MissingJavadocType")
+@SpringBootApplication
+public class Wuffer2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Wuffer2Application.class, args);
+  }
+
+}
---
+++ src/test/java/de/propra/wuffer2/Wuffer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.propra.wuffer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wuffer2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
