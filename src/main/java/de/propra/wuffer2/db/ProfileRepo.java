409859657ea9d9d33a1a9227c71cec50c491e90a
Sascha Klassen <102946831+sakla106@users.noreply.github.com>
2023-02-07 12:38:51 +0100

    adds @Repository annotation to repos

--- src/main/java/de/propra/wuffer2/db/ProfileRepo.java
+++ src/main/java/de/propra/wuffer2/db/ProfileRepo.java
@@ -2,7 +2,9 @@ package de.propra.wuffer2.db;
 
 import de.propra.wuffer2.domain.Profile;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProfileRepo extends ListCrudRepository<Profile, Integer> {
 
 }
--- src/main/java/de/propra/wuffer2/db/WuffRepo.java
+++ src/main/java/de/propra/wuffer2/db/WuffRepo.java
@@ -2,7 +2,9 @@ package de.propra.wuffer2.db;
 
 import de.propra.wuffer2.domain.Wuff;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WuffRepo extends ListPagingAndSortingRepository<Wuff, Integer> {
 
 }
